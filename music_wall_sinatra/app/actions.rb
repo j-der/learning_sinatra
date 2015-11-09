577749b3071fed81ce2449e50917af2ee495ef46
Jas Der <jasmineder@gmail.com>
2015-11-09 21:57:18 +0000

    more progress

--- music_wall_sinatra/app/actions.rb
+++ music_wall_sinatra/app/actions.rb
@@ -7,3 +7,17 @@ get '/songs' do
   @songs = Songs.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Songs.new(
+    title: params[:title],
+    artist: params[:artist],
+    author: params[:author]
+  )
+  @song.save
+  redirect '/songs'
+end
