d9b0611ace0bea6a004cbc03296747e048080879
Jas Der <jasmineder@gmail.com>
2015-11-10 23:13:54 +0000

    Fixed some bugs. Need to implement authentication

--- music_wall_sinatra/app/actions.rb
+++ music_wall_sinatra/app/actions.rb
@@ -1,51 +1,24 @@
 # Homepage (Root path)
 helpers do
   def current_user
-    if session[:id] and user = User.find(session[:id])
-      user
-    end
+    User.find(session[:user]) if session[:user]
   end
 end
 
 get '/' do
   erb :'index'
+  # @user = User.new
+  # redirect '/index'
+end
+
+get '/sign_up' do
   @user = User.new
-  redirect '/songs'
+  erb :'sign_up'
+
 end
 
-get 'sign_up' do
-  @user = User.new
-  redirect '/sign_up'
-end
-
-post '/sign_up' do
-  @user = User.new(
-    username: params[:username],
-    password: params[:password]
-    )
-  if @user.save
-    session[:id] = @user.id
-    redirect '/songs/index'
-  else
-    redirect '/songs'
-  end
-end
-
-post '/index' do
-  if @user = User.find_by(username: params[:username])
-    # logged_in = user.authenticate(params[:password]) this is used when you use the password digest thing
-      # if logged_in
-      # session[:user] = @user.id
-    # else
-    #   error message
-    if @user.password == params[:password]
-      session[:id] = @user.id
-      redirect '/songs'
-    end
-  else
-    @error = "Nuh-uh-uh, you didn't say the magic word!"
-    redirect 'songs/index'
-  end
+get 'index' do
+  erb :'index'
 end
 
 get '/songs' do
@@ -77,3 +50,33 @@ end
 
 # Look into REST for doing voting etc
 # activerecrod passwords
+post '/index' do
+  if @user = User.find_by(username: params[:username])
+    # logged_in = user.authenticate(params[:password]) this is used when you use the password digest thing
+      # if logged_in
+      # session[:user] = @user.id
+    # else
+    #   error message
+    if @user.password == params[:password]
+      session[:id] = @user.id
+      redirect '/songs/all_songs'
+    end
+  else
+    @error = "Nuh-uh-uh, you didn't say the magic word!"
+    redirect '/songs/all_songs'
+  end
+
+end
+
+post '/sign_up' do
+  @user = User.new(
+    username: params[:username],
+    password: params[:password]
+    )
+  if @user.save
+    session[:id] = @user.id
+    redirect '/index'
+  else
+    redirect '/songs'
+  end
+end
